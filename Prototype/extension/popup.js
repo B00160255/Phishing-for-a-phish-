d46c2031e98e03fbba453317bf4e5f9f16d8d32d
amyoneill <b00161543@mytudublin.ie>
2026-08-11 15:18:59 +0100

    Re organising directories
    - moved words db from Prototype/backend to the main directory (the version of it that was there was empty)
    -deleted Prototype dir as it was not being used

--- Prototype/extension/popup.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('scanBtn').onclick = async () => {
-  let res = await fetch('http://127.0.0.1:5000/scan', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ sender_domain: document.getElementById('senderDomain').value, email_text: document.getElementById('emailText').value })
-  });
-  let data = await res.json();
-  document.getElementById('result').innerText = `Verdict: ${data.colour} | Score: ${data.score}\nReason: ${data.reason}`;
-};
